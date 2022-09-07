55bb7c60159d0aedff1eb5143a5b2d797ad3a5e4
bruno00o <bseilliebert@outlook.fr>
2022-09-07 21:16:35 +0200

    auto dl calendar

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ const cors = require('cors');
 const cron = require('node-cron');
 const fs = require('fs');
 const https = require('https');
-const ical = require('node-ical');
 const app = express();
 const port = 8080;
 
@@ -33,50 +32,33 @@ app.use('/students', studentsRouter);
 app.use('/admin', adminRouter);
 app.use('/friends', friendsRouter);
 
-// cron.schedule("*/10 * * * * *", function() {
-//     let url = "";
-//     let file = fs.createWriteStream("calendar.ics");
-//     let request = https.get(url, function(response) {
-//         console.log("Downloading calendar...");
-//         response.pipe(file);
-//         file.on('finish', function() {
-//             file.close();
-//             console.log("Calendar downloaded.");
-//         });
-//     }).on('error', function(err) {
-//         console.log("Error while downloading calendar.");
-//         fs.unlink(dest);
-//     });
-// });
-
-cron.schedule("*/10 * * * * *", function() {
-    let url = "";
-    ical.async.fromURL(url).then(function(calendar) {
-        console.log("Downloading calendar...");
-        let calendarObject = {};
-        for (let key in calendar) {
-            if (calendar.hasOwnProperty(key)) {
-                let event = calendar[key];
-                if (event.type === 'VEVENT') {
-                    let date = new Date(event.start);
-                    let day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
-                    let month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
-                    let year = date.getFullYear();
-                    let dateString = day + '/' + month + '/' + year;
-                    calendarObject[dateString] = event.summary;
-                }
+cron.schedule("*/10 * * * * *", function () {
+    let fileURLs = "./src/calendar/calendarURLs.json";
+    let calendarFolder = "./src/calendar/";
+    fs.readFile(fileURLs, (err, data) => {
+        if (err) {
+            console.log("Error while reading calendarURLs.json");
+        } else {
+            let calendarURLs = JSON.parse(data);
+            let calendars = {};
+            for (cal in calendarURLs) {
+                let url = calendarURLs[cal].url;
+                let file = fs.createWriteStream(calendarFolder + calendarURLs[cal].icsFileName);
+                let request = https.get(url, function (response) {
+                    console.log("Downloading calendar...");
+                    response.pipe(file);
+                    file.on('finish', function () {
+                        file.close();
+                        console.log("Calendar downloaded.");
+                    });
+                }).on('error', function (err) {
+                    console.log("Error while downloading calendar.");
+                    fs.unlink(dest);
+                });
             }
         }
-        fs.writeFile('calendar.json', JSON.stringify(calendarObject), function(err) {
-            if (err) {
-                console.log("Error while downloading calendar.");
-            } else {
-                console.log("Calendar downloaded.");
-            }
-        });
     });
 });
-
 
 app.listen(port);
 
