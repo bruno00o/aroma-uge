d0ad3b25992e14d8937b2fe17a122f8d5cc81442
bruno00o <bseilliebert@outlook.fr>
2022-09-07 21:31:25 +0200

    change cron to 02:00

--- api/index.js
+++ api/index.js
@@ -15,6 +15,9 @@ const studentsRouter = require('./router/studentsRouter');
 const adminRouter = require('./router/adminRouter');
 const friendsRouter = require('./router/friendsRouter');
 
+var access = fs.createWriteStream('./access.log', { flags: 'a' })
+    , error = fs.createWriteStream('./error.log', { flags: 'a' });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -32,7 +35,7 @@ app.use('/students', studentsRouter);
 app.use('/admin', adminRouter);
 app.use('/friends', friendsRouter);
 
-cron.schedule("*/10 * * * * *", function () {
+cron.schedule("0 2 * * * * ", function () {
     let fileURLs = "./src/calendar/calendarURLs.json";
     let calendarFolder = "./src/calendar/";
     fs.readFile(fileURLs, (err, data) => {
@@ -45,14 +48,13 @@ cron.schedule("*/10 * * * * *", function () {
                 let url = calendarURLs[cal].url;
                 let file = fs.createWriteStream(calendarFolder + calendarURLs[cal].icsFileName);
                 let request = https.get(url, function (response) {
-                    console.log("Downloading calendar...");
                     response.pipe(file);
                     file.on('finish', function () {
                         file.close();
-                        console.log("Calendar downloaded.");
+                        access.write("Downloaded " + calendarURLs[cal].icsFileName + " at " + new Date() + "\n");
                     });
                 }).on('error', function (err) {
-                    console.log("Error while downloading calendar.");
+                    error.write("Error while downloading " + calendarURLs[cal].icsFileName + " at " + new Date() + "\n");
                     fs.unlink(dest);
                 });
             }
